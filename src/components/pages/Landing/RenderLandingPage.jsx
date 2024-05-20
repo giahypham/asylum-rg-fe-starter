73f6f333f8697776e814d3e06004b6a601b46b5b
giahypham <andygpham@gmail.com>
2024-05-20 17:04:53 -0400

    Completed Ticket 1: Landing Page

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 // for the purposes of testing PageNav
-// import PageNav from '../../common/PageNav';
+import PageNav from '../../common/PageNav';
 
 function RenderLandingPage(props) {
   const scrollToTop = () => {
@@ -32,15 +32,40 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
-      <div className="view-more-data-btn-container">
-        <Button
-          type="default"
-          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
-          onClick={() => history.push('/graphs')}
-        >
-          View the Data
-        </Button>
+      <div className='graphs-section'> 
+        <div className='graph-office'>
+          <img src={GrantRatesByOfficeImg} alt='Search Grant Rates By Office'/>
+          <h3>Search Grant Rates By Office</h3>
+        </div>
+        <div className='graph-nationality'>
+          <img src={GrantRatesByNationalityImg} alt='Search Grant Rates By Nationality'/>
+          <h3>Search Grant Rates By Nationality</h3>
+        </div>
+        <div className='graph-time'>
+          <img src={GrantRatesOverTimeImg} alt='Search Grant Rates Over Time'/>
+          <h3>Search Grant Rates Over Time</h3>
+        </div>
+      </div>
+
+      <div className='view-more-and-download-data-btn-container'> {/*added a container for both buttons to use flexbox */}
+        <div className="view-more-data-btn-container">
+          <Button
+            type="default"
+            style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+            onClick={() => history.push('/graphs')}
+          >
+            View the Data
+          </Button>
+        </div>
+        <div className="download-data-btn-container">
+          <Button
+            type="default"
+            style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+            onClick={() => history.push('/data')}
+          >
+            Download the Data
+          </Button>
+        </div>
       </div>
 
       <div className="middle-section">
@@ -59,9 +84,39 @@ function RenderLandingPage(props) {
           </h3>
         </div>
       </div>
+
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <div className='bottom-section-header-container'>
+            <h1>Systemic Disparity Insights</h1>
+          </div>
+          <div className='bottom-section-stats-container'>
+            <div className='stats-one-container'>
+              <h3>36%</h3>
+              <p>By the end of the Trump administration, the average asylum office grant rate had fallen 36 percent from an average of 44 percent in fiscal year 2016 to 28 percent in fiscal year 2020.</p>
+            </div>
+            <div className='stats-two-container'>
+              <h3>5%</h3>
+              <p>The New York asylum office grant rate dropped to 5 percent in fiscal year 2020.</p>
+            </div>
+            <div className='stats-three-container'>
+              <h3>6x Lower</h3>
+              <p>Between fiscal year 2017 and 2020, the New York asylum office’s average grant rate was six times lower than the San Francisco asylum office.</p>
+            </div>
+          </div>
+        </div>
+        
+        <div className="read-more-btn-container">
+          <Button
+            type="default"
+            style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+            onClick={() => history.push('/readmore')}
+          >
+            Read More
+          </Button>
+        </div>
+
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
